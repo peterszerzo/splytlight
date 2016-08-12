cbdd1d7737d36e1699d96fa77cd0471788de4f01
Peter Szerzo <peter.szerzo@airtame.com>
2016-08-12 08:47:42 +0200

    Added control circles. Refactored unit offset logic

--- src/components/unit.js
+++ src/components/unit.js
@@ -6,36 +6,63 @@ import {splytUnit} from '../constants/geometries';
 
 const offset = 10;
 
-export default function Unit() {
+function Controls() {
   const {baseHeight, armLength, armAngle} = splytUnit;
   return (
-    h('g', {
-      namespace: svgNameSpace,
-      attributes: {
-        stroke: '#000',
-        'stroke-width': '6',
-        'stroke-linecap': 'round',
-        'stroke-linejoin': 'round'
-      }
-    }, [
+    h('g', {namespace: svgNameSpace, attributes: {
+      stroke: 'none',
+      fill: 'rgba(32, 26, 22)',
+    }}, [
+      h('circle', {namespace: svgNameSpace, attributes: {
+        x2: - (armLength - offset / 2) * Math.cos(armAngle),
+        y2: baseHeight + (armLength - offset / 2) * Math.sin(armAngle),
+        r: baseHeight / 4
+      }}),
+      h('circle', {namespace: svgNameSpace, attributes: {
+        x2: + (armLength - offset / 2) * Math.cos(armAngle),
+        y2: baseHeight + (armLength - offset / 2) * Math.sin(armAngle),
+        r: baseHeight / 4
+      }})
+    ])
+  );
+}
+
+function Lines() {
+  const {baseHeight, armLength, armAngle} = splytUnit;
+  return (
+    h('g', {namespace: svgNameSpace, attributes: {
+      stroke: '#638FBE',
+      'stroke-width': '6',
+      'stroke-linecap': 'round',
+      'stroke-linejoin': 'round'
+    }}, [
       h('line', {namespace: svgNameSpace, attributes: {
         x1: 0,
-        y1: offset,
+        y1: offset / 2,
         x2: 0,
         y2: baseHeight
       }}),
       h('line', {namespace: svgNameSpace, attributes: {
         x1: 0,
         y1: baseHeight,
-        x2: - armLength * Math.cos(armAngle),
-        y2: baseHeight + armLength * Math.sin(armAngle)
+        x2: - (armLength - offset / 2) * Math.cos(armAngle),
+        y2: baseHeight + (armLength - offset / 2) * Math.sin(armAngle)
       }}),
       h('line', {namespace: svgNameSpace, attributes: {
         x1: 0,
         y1: baseHeight,
-        x2: + armLength * Math.cos(armAngle),
-        y2: baseHeight + armLength * Math.sin(armAngle)
-      }}),
+        x2: + (armLength - offset / 2) * Math.cos(armAngle),
+        y2: baseHeight + (armLength - offset / 2) * Math.sin(armAngle)
+      }})
+    ])
+  );
+}
+
+export default function Unit() {
+  return (
+    h('g', {namespace: svgNameSpace}, [
+      Lines(),
+      Controls()
     ])
   );
 }
