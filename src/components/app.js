cee9844ef5e8a5164d1f32afcf8565a7da7b00fd
Peter Szerzo <peter.szerzo@airtame.com>
2016-08-13 10:28:25 +0200

    Add split-column layout

--- src/components/app.js
+++ src/components/app.js
@@ -5,12 +5,19 @@ import UnitsContainer from './units-container';
 export default function App({data}, {setState}) {
   return (
     h('div', {className: 'app'}, [
-      h('h1', {className: 'app__title'}, 'SplytViz'),
-      UnitsContainer({
-        data: data,
-        x: 500,
-        y: 500
-      }, {setState})
+      h('div', {className: 'app__main'}, [
+        h('div', {className: 'app__viz'}, [
+          UnitsContainer({
+            data: data,
+            x: 500,
+            y: 500
+          }, {setState})
+        ]),
+        h('div', {className: 'app__viz'}, [])
+      ]),
+      h('div', {className: 'app__nav'}, [
+        h('h1', {}, 'Your very own Splyt Light')
+      ])
     ])
   );
 }
