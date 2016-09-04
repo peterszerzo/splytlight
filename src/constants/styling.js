5b4093710c0a05646e195ba91b96f0950dbd5d86
Peter Szerzo <szerzo.peter@gmail.com>
2016-09-04 14:36:42 +0200

    Add green and red colors

--- src/constants/styling.js
+++ src/constants/styling.js
@@ -1,6 +1,8 @@
 export const blue = '#638FBE';
 export const brown = '#201A16';
 export const white = '#FFFFFF';
+export const green = '#32A621';
+export const red = '#BF0010';
 
 export const strokeWeight = 6;
 export const controlCircleRadius = 10;
