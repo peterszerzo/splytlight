be9641421768de4f224a3bb1235e281ea6627196
Peter Szerzo <szerzo.peter@gmail.com>
2016-11-13 11:41:13 +0100

    Generalized 3d geometry

--- src/three-app/splyt/index.js
+++ src/three-app/splyt/index.js
@@ -3,17 +3,26 @@ import {Group} from 'three';
 import {splyt} from '../../constants/geometries';
 import createSplytUnit from './unit';
 
-function xformSplyt(object, size, direction){
+const {PI, sin, cos} = Math;
+
+function transformSplyt(object, size, direction){
   const armLength = size.armLength;
   const bHeight = size.baseHeight;
   const armAngle = size.armAngle;
   if (direction === 'left') {
-    object.rotateZ(Math.PI / 2 - armAngle);
-    object.position.set( - 0.5 * armLength, bHeight + 0.5 * armLength * Math.sqrt(3), 0);
-  }
-  else{
-    object.rotateZ(- (Math.PI / 2 - armAngle));
-    object.position.set( + 0.5 * armLength, bHeight + 0.5 * armLength * Math.sqrt(3), 0);
+    object.rotateZ(PI / 2 - armAngle);
+    object.position.set(
+      - armLength * cos(armAngle),
+      bHeight + armLength * sin(armAngle),
+      0
+    );
+  } else {
+    object.rotateZ(- (PI / 2 - armAngle));
+    object.position.set(
+      armLength * cos(armAngle),
+      bHeight + armLength * sin(armAngle),
+      0
+    );
   }
 }
 
@@ -22,9 +31,9 @@ function createSplytTree(group, state) {
     return group;
   } else {
     const leftGroup = createSplytTree(new Group(), state.left);
-    xformSplyt(leftGroup, splyt[state.size], 'left');
+    transformSplyt(leftGroup, splyt[state.size], 'left');
     const rightGroup = createSplytTree(new Group(), state.right);
-    xformSplyt(rightGroup, splyt[state.size], 'right');
+    transformSplyt(rightGroup, splyt[state.size], 'right');
     group.add(leftGroup);
     group.add(rightGroup);
     group.add(createSplytUnit(splyt[state.size]));
